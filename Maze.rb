50046dc52891f0bb35ea1421e670077e5be9ee7b
Luka Milekic <lmilekic@gmail.com>
2014-02-11 11:15:27 -0500

    final
    changes for nicer output

--- Maze.rb
+++ Maze.rb
@@ -26,8 +26,15 @@ class Maze
 	end
 end
 
+puts "creating new 4 x 4 maze"
 m = Maze.new(4, 4)
+puts "loading in the maze 111111111100010001111010101100010101101110101100000101111011101100000101111111111"
 m.load("111111111100010001111010101100010101101110101100000101111011101100000101111111111")
+puts "displaying maze"
+m.display
+puts "creating new mazeSolver object to solve maze"
 ms = MazeSolver.new(m)
+"is 1,1 to 1,4 solveable?"
 puts ms.solve(1,1,1,4)
+puts "Final maze"
 m.display()
